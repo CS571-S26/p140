aa6593a50c794ffe17ba30b4720e19fe19c77e1f
Jackson Sprenger <jsprenger@wisc.edu>
2026-04-21 21:59:44 -0500

    update contact page

--- src/components/pages/AboutPageContent.jsx
+++ src/components/pages/AboutPageContent.jsx
@@ -13,7 +13,7 @@ export default function AboutPageContent() {
         </Col>
       </Row>
 
-      <Row xs={1} md={2} className="about-page__grid">
+      <Row xs={1} md={2} lg={3} className="about-page__grid">
         <Col>
           <Card className="about-page__panel">
             <Card.Body>
@@ -32,6 +32,46 @@ export default function AboutPageContent() {
                 <ListGroup.Item>Minecraft Mods</ListGroup.Item>
                 <ListGroup.Item>A plan to replace microsoft</ListGroup.Item>
                 <ListGroup.Item>A new way to blow bubbles</ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="about-page__panel">
+            <Card.Body>
+              <Card.Title>Find Me Online</Card.Title>
+              <Card.Text className="about-page__contact-intro">
+                Connect with me through these links.
+              </Card.Text>
+              <ListGroup variant="flush" className="about-page__list">
+                <ListGroup.Item>
+                  <a
+                    href="https://www.linkedin.com/in/your-linkedin"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="about-page__contact-link"
+                  >
+                    LinkedIn
+                  </a>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <a
+                    href="https://github.com/your-github"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="about-page__contact-link"
+                  >
+                    GitHub
+                  </a>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <a
+                    href="mailto:you@example.com"
+                    className="about-page__contact-link"
+                  >
+                    Email
+                  </a>
+                </ListGroup.Item>
               </ListGroup>
             </Card.Body>
           </Card>
